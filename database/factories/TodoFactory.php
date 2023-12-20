66785557f0bf9f411e84f1023f1f4de303ca7ac3
Trưởng Phạm <truong.pham210214@vnuk.edu.vn>
2023-12-21 00:36:09 +0700

    seed database for categories and todos

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'todo' => fake()->paragraph(),
+            'is_completed' => fake()->boolean(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
